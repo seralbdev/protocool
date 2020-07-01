d15ef289396326cb6ab3a4885b833cb1ec161ce0
seralbdev <seralbdev@gmail.com>
2020-07-01 19:53:55 +0200

    Moving fwd with encoder

--- src/seralbdev/protocool/bseq-encoder.clj
+++ src/seralbdev/protocool/bseq-encoder.clj
@@ -1,2 +1,23 @@
-(ns seralbdev.protocool.bseq-encoder)
+(ns seralbdev.protocool.bseq-encoder
+  (:require
+   [seralbdev.protocool.base :as b]
+   [seralbdev.protocool.bseq :as d]))
 
+(defn- write-str! [stream value]
+  (let [bdata (.getBytes value)
+        len (count bdata)]
+    (b/write-bytes! stream bdata 0 len)))
+
+
+(defn- write-pstr! [stream pfx value]
+  (let [len (count value)]
+    (cond
+      (= pfx ::d/i8) (b/write-byte! stream len)
+      (= pfx ::d/i16) (b/write-int16! stream len)
+      (= pfx ::d/i32) (b/write-int32! stream len))
+    (write-str! stream value)))
+   
+
+(defn- process-pstr [stream rank pfx data]
+  (if (nil? rank) (write-pstr! stream pfx data)
+      (run! #(write-pstr! stream pfx %) (take rank data))))
--- src/seralbdev/protocool/bseq.clj
+++ src/seralbdev/protocool/bseq.clj
@@ -1,1 +1,6 @@
-(ns seralbdev.protocool.bseq)
+(ns seralbdev.protocool.bseq
+  (:require
+   [clojure.spec.alpha :as s]))
+
+(s/def ::valid-tokens #{::bool ::i8 ::u8 ::i16 ::u16 ::i32 ::u32 ::i64 ::u64 ::r32 ::r64 ::str ::pstr ::struct ::padding})
+
