eddcd1235306b515691e513bd91b16d022ba050b
seralbdev <seralbdev@gmail.com>
2020-07-05 23:53:53 +0200

    more progress in encoder

--- src/seralbdev/protocool/bseq_encoder.clj
+++ src/seralbdev/protocool/bseq_encoder.clj
@@ -8,7 +8,6 @@
         len (count bdata)]
     (b/write-bytes! stream bdata 0 len)))
 
-
 (defn- write-pstr! [stream pfx value]
   (let [len (count value)]
     (cond
@@ -18,21 +17,30 @@
     (write-str! stream value)))
 
 (defn- process-pstr! [stream fmeta data]
-  (let [{rank ::d/rank pfx ::d/pfx} fmeta]
-      (if (nil? rank) (write-pstr! stream pfx data)
-        (run! #(write-pstr! stream pfx %) (take rank data)))))
+  (let [{pfx ::d/pfx} fmeta]
+    (write-pstr! stream pfx data)))
 
 (defn- process-str! [stream fmeta data]
-  (let [{rank ::d/rank len ::d/len} fmeta]
-    (if (nil? rank) (write-str! stream (subs data 0 len))
-      (run! #(write-str! stream (subs % 0 len)) (take rank data)))))
+  (let [{len ::d/len} fmeta]
+    (write-str! stream (subs data 0 len))))
 
-(defn- process-item! [stream item data]
+(defn- process-item! [stream fmeta data f]
+  (let [{rank ::d/rank} fmeta]
+    (if (nil? rank) (f stream fmeta data)
+      (run! #(f stream fmeta %) (take rank data)))))
+
+(defn- dispatch-item! [stream item data]
   (let [[_ ftype fmeta] item]
     (cond
-      (= ftype ::d/str) (process-str! stream fmeta data)
-      (= ftype ::d/pstr) (process-pstr! stream fmeta data))))
+      (= ftype ::d/i8) (process-item! stream fmeta data #(b/write-byte! %1 %3))
+      (= ftype ::d/i16) (process-item! stream fmeta data #(b/write-int16! %1 %3))
+      (= ftype ::d/i32) (process-item! stream fmeta data #(b/write-int32! %1 %3))
+      (= ftype ::d/i64) (process-item! stream fmeta data #(b/write-int64! %1 %3))
+      (= ftype ::d/r32) (process-item! stream fmeta data #(b/write-real32! %1 %3))
+      (= ftype ::d/r64) (process-item! stream fmeta data #(b/write-real64! %1 %3))
+      (= ftype ::d/str) (process-item! stream fmeta data process-str!)
+      (= ftype ::d/pstr) (process-item! stream fmeta data process-pstr!))))
 
 (defn write! [stream pseq data]
   (let [pairs (partition 2 (interleave pseq data))]
-    (run! #(process-item! stream (first %) (last %)) pairs)))
+    (run! #(dispatch-item! stream (first %) (last %)) pairs)))
--- test/protocool/bseq_encoder_test.clj
+++ test/protocool/bseq_encoder_test.clj
@@ -4,7 +4,7 @@
             [seralbdev.protocool.bseq :as d]
             [seralbdev.protocool.bseq_encoder :as enc]))
 
-(t/deftest process-pstr1
+(t/deftest process-pstr
   (let [bs (b/create)
         pseq [["F1" ::d/pstr {::d/pfx ::d/i16}]]
         data ["HiWorld!"]
@@ -12,3 +12,70 @@
         data (b/seal! bs)]
     (t/is (= (seq data) [0 8 72 105 87 111 114 108 100 33]) true)))
 
+(t/deftest process-str
+  (let [bs (b/create)
+        pseq [["F1" ::d/str {::d/len 8}]]
+        data ["HiWorld!xx"]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)]
+    (t/is (= (seq data) [72 105 87 111 114 108 100 33]) true)))
+
+(t/deftest process-i8
+  (let [bs (b/create)
+        pseq [["F1" ::d/i8]]
+        data [69]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)]
+    (t/is (= (seq data) [69]) true)))
+
+(t/deftest process-i8s
+  (let [bs (b/create)
+        pseq [["F1" ::d/i8 {::d/rank 3}]]
+        data [[69 70 71]]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)]
+    (t/is (= (seq data) [69 70 71]) true)))
+
+(t/deftest process-i16
+  (let [bs (b/create)
+        pseq [["F1" ::d/i16]]
+        data [69]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)]
+    (t/is (= (seq data) [0 69]) true)))
+
+(t/deftest process-i32
+  (let [bs (b/create)
+        pseq [["F1" ::d/i32]]
+        data [69]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)]
+    (t/is (= (seq data) [0 0 0 69]) true)))
+
+(t/deftest process-i64
+  (let [bs (b/create)
+        pseq [["F1" ::d/i64]]
+        data [69]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)]
+    (t/is (= (seq data) [0 0 0 0 0 0 0 69]) true)))
+
+(t/deftest process-r32
+  (let [bs (b/create)
+        pseq [["F1" ::d/r32]]
+        data [69.71]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)
+        rbs (b/wrap-bytearray data)
+        value (b/read-real32! rbs)]
+    (t/is (and (> value 69.70) (< value 69.72)))))
+
+(t/deftest process-r64
+  (let [bs (b/create)
+        pseq [["F1" ::d/r64]]
+        data [69.71]
+        _ (enc/write! bs pseq data)
+        data (b/seal! bs)
+        rbs (b/wrap-bytearray data)
+        value (b/read-real64! rbs)]
+    (t/is (and (> value 69.70) (< value 69.72)))))
