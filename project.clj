a3684f4b99ac237e0bf44b01f8595e8fc34a5ba8
seralbdev <46577538+seralbdev@users.noreply.github.com>
2021-11-28 17:59:59 +0100

    small issue

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.netty/netty-buffer "4.1.42.Final"]]
-  :repl-options {:init-ns seralbdev.protocool.core})
+  :repl-options {:init-ns seralbdev.protocool.base})
